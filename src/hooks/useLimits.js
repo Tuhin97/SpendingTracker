db23437a348bbef1b094185d856d140c7b402376
Tuhin Anand <tuhinanand3@gmail.com>
2026-04-04 01:36:07 +1030

    Added logic to set and retrieve the weekly limit from memory.

--- src/hooks/useLimits.js
+++ src/hooks/useLimits.js
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STORAGE_KEY = '@spend_limit';
+const SAVINGS_KEY = '@savings_goal';
 
 export function useLimits() {
   const [limit, setLimitState] = useState(null);
+  const [savingsGoal, setSavingsGoalState] = useState(null);
 
-  useEffect(() => {
+    useEffect(() => {
     AsyncStorage.getItem(STORAGE_KEY).then((val) => {
       if (val) setLimitState(parseFloat(val));
+    });
+    AsyncStorage.getItem(SAVINGS_KEY).then((val) => {
+      if (val) setSavingsGoalState(parseFloat(val));
     });
   }, []);
 
@@ -17,5 +22,12 @@ export function useLimits() {
     await AsyncStorage.setItem(STORAGE_KEY, String(value));
   }
 
-  return { limit, setLimit };
+  async function setSavingsGoal(value) {
+    setSavingsGoalState(value);
+    await AsyncStorage.setItem(SAVINGS_KEY, String(value));
+  }
+
+
+  return { limit, setLimit, savingsGoal, setSavingsGoal };
+
 }
